700669e85502a8a68ab8adefd0b0790264b3603e
cesine <cesine@host>
2011-04-26 12:45:12 -0400

    modified to work with corpora directory

--- src/com/fieldlinguist/groovy/Srt2Dialogs.groovy
+++ src/com/fieldlinguist/groovy/Srt2Dialogs.groovy
@@ -1,100 +1,61 @@
-/*File access from http://pleac.sourceforge.net/pleac_groovy/fileaccess.html
-
-// test values (change for your os and directories)
-inputPath='Pleac/src/pleac7.groovy'; outPath='Pleac/temp/junk.txt'
-
-// For input Java uses InputStreams (for byte-oriented processing) or Readers
-// (for character-oriented processing). These can throw FileNotFoundException.
-// There are also other stream variants: buffered, data, filters, objects, ...
-inputFile = new File(inputPath)
-inputStream = new FileInputStream(inputFile)
-reader = new FileReader(inputFile)
-inputChannel = inputStream.channel
-
+/*TODO: generate meta information about the subtitles such as author, publisher and title. could use the
+			web to get the information by taking a snippet of text and googling for it.
 */
-
-//
 try {
-	subtitlespath=opentextsource()
-	sourcefile = new FileReader(subtitlespath)
-
+	String NonPublicCorpora = "/Users/gina/Documents/workspacests/NonPublicCorpora/"
 	/*
-	 * Print the lines which contain the word, breaking on the word chapter
-	 *
-	 * /using withWriter() always closes the File, whatever is thrown inside
-		//closure...
-		try{
-		  new File('TestFile3.txt').withWriter(){ w->
-	 */
-	/*Fetch the last element of a list
-		Given the list [Red, Green, Blue], access the last element ('Blue')
-		groovy
-		list = ['Red', 'Green', 'Blue']
-		result = list[-1]
-		
-		so in line below, it splits the subtitles path based on either windows \ or linux / slashes,
-		and takes the last element to be the filename of the subtitles
-		TODO: generate meta information abou the subtitles such as author, publisher and title. could use the
-		web to get the information by taking a snippet of text and googling for it.
-		*/
-	def subtitlesfilename = subtitlespath.split(/[\\\/]/)[-1]
-	def albumfolder=subtitlesfilename.replaceAll(/\.srt$/,'')
-		
-	def outpath = "gen/${albumfolder}"
-	new File(outpath).mkdir()
-	println "The subtitles results will be created in this directory: ${albumfolder}"
+	 * get all files in directory containing substring, 
+	 * for each file, extract the dialogs
+	 * 
+	 */	
+	def subtitlesDir = new File(NonPublicCorpora+'subtitles/')
+	files = subtitlesDir.listFiles().grep(~/.*Megamind.*srt$/)
+	def title="Megamind_Movie"
+	for(currentFile in files){	
+		sourcefile = new FileReader(currentFile)
 	
-	def out = new FileWriter("${outpath}/Dialogs.txt")
-	try{
+		/*Fetch the last element of a list
+			Given the list [Red, Green, Blue], access the last element ('Blue')
+			result = list[-1]
+			*/
+		def subtitlesfilename = currentFile.toString().split(/[\\\/]/)[-1]
+		def albumfolder=title
+		def outpath = "gen/${albumfolder}"
+		new File(outpath).mkdir()
+		println "The subtitles results will be created in this directory: ${albumfolder}"
+		/*
+		 * Creates 1 file of dialogs per episode
+		 */
+		def out = new FileWriter("${outpath}/${subtitlesfilename}_Dialogs.txt")
 		try{
-			sourcefile.eachLine{
-				if (it =~ /^\d\d*$/){
-					//println "This is the subtitle number: $it"
-					out.print "\n"
-					out.print it.trim()
+			try{
+				sourcefile.eachLine{
+					if (it =~ /^\d\d*$/){
+						//println "This is the subtitle number: $it"
+						out.print "\n"
+						out.print it.trim()
+					}
+					if (it =~ /[A-Za-z]/){
+						//it = checksubripmistakes(it)
+						out.print "\t"
+						out.print it.trim()
+					}else{
+						//println "This should be numbers: $it"
+					}
 				}
-				if (it =~ /[A-Za-z]/){
-					it = checksubripmistakes(it)
-					out.print "\t"
-					out.print it.trim()
-				}else{
-					//println "This should be numbers: $it"
-				}
-				/* Quick way to get text separated from numbers
-				 * if (it =~ /[A-Za-z]/){
-					out.println it
-				}else{
-					println "This should be numbers: $it"
-				}
-				 */
+			}finally{
+				out.close()
 			}
-		}finally{
-			out.close()
-		}
-	}catch(e) { }
-	
-	
+		}catch(e) { }		
+	}//end loop for files
 }catch (Exception ex) {
 	System.err.println(ex.message)
 }
+/*
+ * Below are helping functions
+ */
 
-//return outpath
 
-def opentextsource(){
-	/*TODO get better logic to get filename from user
-	prompt = '\n> '
-	print 'Enter filename:' + prompt
-	new BufferedReader(new InputStreamReader(System.in)).eachLine{ line ->
-		
-		println "Read: $line" // normal output to System.out
-	}
-		/*
-		 * end logic to get filename from user
-		 */
-		
-		return 'res/subtitles/House.S01E04.DVDRip.XviD-FoV.srt'
-	
-}
 /*
  * Checking for systematic mistakes made when the subtitles are from SubRip, an OCR subtitler which recognizes images and turns
  * them into text. Due to Subtitle font being in Arial, SubRip often mixes up 
