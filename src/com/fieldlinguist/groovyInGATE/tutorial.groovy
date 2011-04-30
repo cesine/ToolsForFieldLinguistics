a2588583449ac75992cd156499d0a6c001bafb45
cesine <cesine@host>
2011-04-30 07:28:38 -0400

    regular expressions

--- src/com/fieldlinguist/groovyInGATE/tutorial.groovy
+++ src/com/fieldlinguist/groovyInGATE/tutorial.groovy
@@ -14,42 +14,55 @@ assert(words.size()>0)
 /*
 Print the size of the word set, just for fun to see how many words we have to work with...
 */
-print (words.size())
+print "This is how many words we have "+ (words.size())
 
 
 /*
 Step 2: prepare your map to hold the word:frequency pairs
 ex: the:388  Means that the word "the" appears 388 times in the document
+
+
+While we are debugging, lets only loop through part of the words! Otherwise we might have to wait
+for 10 minutes every time we run it. 
 */
-
 def frequencyMap = [:]
-
-def numberToStopTheLoopToShowOnlyPartOfIt = 0
+def numberToStopTheLoopToShowOnlyPartOfIt = 0                 //(to only run part of the loop)
 /*
 Loop through the words
 */
 for(wordObject in words){
-    numberToStopTheLoopToShowOnlyPartOfIt ++
-    if(numberToStopTheLoopToShowOnlyPartOfIt >5){ break; }
+    numberToStopTheLoopToShowOnlyPartOfIt ++                 //(to only run part of the loop)
+    if(numberToStopTheLoopToShowOnlyPartOfIt >15){ break; }   //(to only run part of the loop)
     
-    //The words come out as objects from gate(with a lot of values...)
-    print "\n"+wordObject
+    //we just want the string of the word
     word = wordObject.getFeatures().get("string")
     //make the word lowercase, this is optional it depends on what your goal is.
     word = word.toLowerCase()
-
-    /*
-     *  If: the word isnt in the map, set its value to 1 because its the first occurrence
-     *  Otherwise: increase its value because we just saw it again
-     */
-    if (null == frequencyMap[word]) {
-                frequencyMap[word] = 1
-    } else {
-                frequencyMap[word]++
-    }
     
+    /*
+    get rid of non-words, only use words that have an lowercase or uppercase letter in the middle 
+    For more info: Google: regular expressions groovy
+    ( Could do anything that is only letters: [a-z,A-Z]* but this is a bad assumption for IPA or romanized arabic chat, or passamaquoddy where numbers are used to represent sounds
+    */
+    if (word ==~ /.+[a-z,A-Z].*/){
+        //its okay, so process it
+        
+        /*
+         *  If: the word isnt in the map, set its value to 1 because its the first occurrence
+         *  Otherwise: increase its value because we just saw it again
+         */
+        if (null == frequencyMap[word]) {
+                frequencyMap[word] = 1
+        } else {
+                frequencyMap[word]++
+        }
+    }else{
+        //good to print out to make sure your regular expression is discarding the right junk
+        print "\nDiscarding this as junk: "+word
+    }
+
 }//end for loop to go through all the words
 
-print "This is what the frequency map looks like\n"+ frequencyMap
+print "\n\nAll Done. \n This is what the frequency map looks like\n"+ frequencyMap
 
 
