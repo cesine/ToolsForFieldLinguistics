c7016da8ac45899597d12a74007d69ef94222176
cesine <cesine@yahoo.com>
2013-12-07 16:18:26 -0500

    specs in new folder structure running browser

--- test/javascript/all.js
+++ test/javascript/all.js
@@ -1,7 +1,7 @@
 require("montage-testing").run(require, [
 	// Unit tests: Alphabetical order
-	"test/generate-data-spec",
+	"test/javascript/generate-data-spec",
 
 	// Integration/End to end tests: Alphabetical order
-	"test/ToolsForFieldLinguistics-spec"
+	"test/javascript/ToolsForFieldLinguistics-spec"
 ]);
