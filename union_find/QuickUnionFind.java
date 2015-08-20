660802a2c5f08036bf1c9143071a36377ea6443d
cesine <cesine@yahoo.com>
2015-08-20 18:33:28 -0400

    added a file and runner for quick union find
    $ javac QuickUnionFindTest*.java  && java QuickUnionFindTest

---
+++ union_find/QuickUnionFind.java
@@ -0,0 +1,25 @@
+public class QuickUnionFind {
+  private int[] id;
+
+  public QuickUnionFind(int N) {
+    id = new int[N];
+    for (int i = 0; i < N; i++) {
+      id[i] = i;
+    }
+  }
+
+  public String toString() {
+    if (id == null || id.length == 0) {
+      return "";
+    }
+    String contents = "[";
+    for (int i = 0; i < id.length; i++) {
+      if (i > 0) {
+        contents += "|";
+      }
+      contents += id[i];
+    }
+    return contents+ "]";
+  }
+
+}
---
+++ union_find/QuickUnionFindTest.java
@@ -0,0 +1,9 @@
+public class QuickUnionFindTest {
+
+  public static void main(String[] args) {
+    int size = 4;
+    QuickUnionFind world = new QuickUnionFind(size);
+    System.out.println("Created world size:  " + size + " : " + world);
+
+  }
+}
