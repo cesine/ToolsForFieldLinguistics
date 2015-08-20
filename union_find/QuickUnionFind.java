868f6efbb7a4d15fccffa9bfca2b8322f3460bdd
cesine <cesine@yahoo.com>
2015-08-20 18:53:11 -0400

    added test to complain if large data set
    if you run union you have to go through N elements which is a problem if its a large dataset

--- union_find/QuickUnionFind.java
+++ union_find/QuickUnionFind.java
@@ -1,5 +1,5 @@
 public class QuickUnionFind {
-  
+
   private int[] id;
 
   public QuickUnionFind(int N) {
@@ -11,6 +11,30 @@ public class QuickUnionFind {
 
   public boolean connected(int p, int q) {
     return id[p] == id[q];
+  }
+
+  /**
+   * This requires a loop through all the elements in the graph
+   * which is not good if the size of the graph is large. 
+   * 
+   * @param  {[type]} int p             [description]
+   * @param  {[type]} int q             [description]
+   * @return {[type]}     [description]
+   */
+  public boolean union(int p, int q) {
+    int pid = id[p];
+    int qid = id[q];
+
+    if (id != null && id.length > 9999){
+      System.out.println("WARNING: This algorithm should not be used on large data sets.");
+      return false;
+    }
+    for (int i = 0; i < id.length; i++) {
+      if (id[i] == pid) {
+        id[i] = qid;
+      }
+    }
+    return true;
   }
 
   public String toString() {
--- union_find/QuickUnionFindTest.java
+++ union_find/QuickUnionFindTest.java
@@ -5,7 +5,6 @@ public class QuickUnionFindTest {
   public static void itShouldConstruct() {
     QuickUnionFind world = new QuickUnionFind(size);
     System.out.println("Created world size: " + size + " : " + world);
-
   }
 
   public static void itShouldKnowIfTwoNodesAreConnected() {
@@ -13,7 +12,13 @@ public class QuickUnionFindTest {
     boolean connected = world.connected(1, 3);
     assert connected == false;
     System.out.println("1 and 3 are connected? " + connected + " " + world);
+  }
 
+  public static void itShouldComplainIfYouRunUnionOnALargeDataSet() {
+    QuickUnionFind world = new QuickUnionFind(10000);
+    boolean ranSucessfully = world.union(1, 3);
+    assert ranSucessfully == false;
+    System.out.println("Should complain. " + !ranSucessfully);
   }
 
   public static void main(String[] args) {
@@ -22,6 +27,7 @@ public class QuickUnionFindTest {
     itShouldConstruct();
     itShouldKnowIfTwoNodesAreConnected();
 
+    itShouldComplainIfYouRunUnionOnALargeDataSet();
     System.out.println("Done\n");
   }
 }
