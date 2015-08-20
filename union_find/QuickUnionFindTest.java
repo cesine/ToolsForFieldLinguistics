e6b735a9a5cb6979a7cd656ee921070b46f1fa47
cesine <cesine@yahoo.com>
2015-08-20 19:11:57 -0400

    tested that connecting nodes was working

--- union_find/QuickUnionFindTest.java
+++ union_find/QuickUnionFindTest.java
@@ -1,14 +1,14 @@
 public class QuickUnionFindTest {
 
-  static int size = 4;
+  static int defaultSize = 4;
 
   public static void itShouldConstruct() {
-    QuickUnionFind world = new QuickUnionFind(size);
-    System.out.println("  Created world size: " + size + " : " + world);
+    QuickUnionFind world = new QuickUnionFind(defaultSize);
+    System.out.println("  Created world defaultSize: " + defaultSize + " : " + world);
   }
 
   public static void itShouldKnowIfTwoNodesAreConnected() {
-    QuickUnionFind world = new QuickUnionFind(size);
+    QuickUnionFind world = new QuickUnionFind(defaultSize);
     boolean connected = world.connected(1, 3);
     assert connected == false;
     System.out.println("  1 and 3 are connected? " + connected + " " + world);
@@ -40,6 +40,17 @@ public class QuickUnionFindTest {
     System.out.println("  Should complain. " + !ranSucessfully);
   }
 
+  public static void itShouldConnectTwoNodes() {
+    QuickUnionFind world = new QuickUnionFind(defaultSize);
+    boolean ranSucessfully = world.union(1, 3);
+    assert ranSucessfully == true;
+    System.out.println("  Should have 1 and 3 be the same " + world);
+
+    world.union(2, 3);
+    System.out.println("  Should have 1, 2 and 3 be the same " + world);
+
+  }
+
   public static void main(String[] args) {
     System.out.println("\nRunning specs: ");
 
@@ -48,6 +59,7 @@ public class QuickUnionFindTest {
 
     // itShouldThrowOutOfMemoryError();
     itShouldComplainIfYouRunUnionOnALargeDataSet();
+    itShouldConnectTwoNodes();
 
     System.out.println("Done\n");
   }
