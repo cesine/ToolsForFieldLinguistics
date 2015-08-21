a7d4c8c0e43a5d2052745365dba4f7c2ba504343
cesine <cesine@yahoo.com>
2015-08-20 21:10:31 -0400

    testing derivation of a complex graph

--- union_find/ConnectedGraphFindTest.java
+++ union_find/ConnectedGraphFindTest.java
@@ -5,6 +5,7 @@ public class ConnectedGraphFindTest {
   public static void itShouldConstruct() {
     ConnectedGraphFind world = new ConnectedGraphFind(defaultSize);
     System.out.println("  Created world defaultSize: " + defaultSize + " : " + world);
+    assert world.toString().equals("[0|1|2|3]");
   }
 
   public static void itShouldBeAbleToTraceTheRootOfANode() {
@@ -16,9 +17,35 @@ public class ConnectedGraphFindTest {
 
   public static void itShouldKnowIfTwoNodesAreConnected() {
     ConnectedGraphFind world = new ConnectedGraphFind(defaultSize);
+    world.union(1, 3);
+    boolean connected = world.connected(1, 3);
+    assert connected == true;
+    System.out.println("  1 and 3 are connected? " + connected + " " + world);
+    assert world.toString().equals("[0|3|2|3]");
+  }
+
+  public static void itShouldKnowIfTwoNodesAreTransitivelyConnected() {
+    ConnectedGraphFind world = new ConnectedGraphFind(defaultSize);
+    boolean ranSucessfully = world.union(2, 3);
+    assert ranSucessfully == true;
+    System.out.println("  Should have 1 and 3 be the same " + world);
+    assert world.toString().equals("[0|1|3|3]");
+
+    world.union(1, 2);
+    System.out.println("  Should have 1, 2 and 3 be the same " + world);
+    assert world.toString().equals("[0|3|3|3]");
+
+    boolean connected = world.connected(1, 3);
+    assert connected == true;
+    System.out.println("  1 and 3 are transitively connected? " + connected + " " + world);
+  }
+
+  public static void itShouldKnowIfTwoNodesAreNotConnected() {
+    ConnectedGraphFind world = new ConnectedGraphFind(defaultSize);
     boolean connected = world.connected(1, 3);
     assert connected == false;
-    System.out.println("  1 and 3 are connected? " + connected + " " + world);
+    System.out.println("  1 and 3 are not connected? " + connected + " " + world);
+    assert world.toString().equals("[0|1|2|3]");
   }
 
   /**
@@ -37,6 +64,7 @@ public class ConnectedGraphFindTest {
     assert ConnectedGraphFind.protectUserFromThemSelves == false;
     ConnectedGraphFind world = new ConnectedGraphFind(999999999);
     System.out.println("  Should have thrown an error. ");
+    assert world.toString().equals("[0|1|2|3]");
     ConnectedGraphFind.protectUserFromThemSelves = true;
   }
 
@@ -44,29 +72,54 @@ public class ConnectedGraphFindTest {
     ConnectedGraphFind world = new ConnectedGraphFind(10000);
     boolean ranSucessfully = world.union(1, 3);
     assert ranSucessfully == false;
-    System.out.println("  Should complain. " + !ranSucessfully);
+    System.out.println("  Should still not initialize a larger than toy data set. " + !ranSucessfully + " " + world);
+    assert world.toString().equals("");
   }
 
-  public static void itShouldConnectTwoNodes() {
-    ConnectedGraphFind world = new ConnectedGraphFind(defaultSize);
-    boolean ranSucessfully = world.union(1, 3);
-    assert ranSucessfully == true;
-    System.out.println("  Should have 1 and 3 be the same " + world);
+  public static void itShouldPartiallyRepresentTheGraphParentNodes() {
+    ConnectedGraphFind world = new ConnectedGraphFind(10);
+    assert world.toString().equals("[0|1|2|3|4|5|6|7|8|9]");
 
-    world.union(2, 3);
-    System.out.println("  Should have 1, 2 and 3 be the same " + world);
+    world.union(1, 0);
+    System.out.println("  " + world);
+    assert world.toString().equals("[0|0|2|3|4|5|6|7|8|9]");
+    world.union(2, 0);
+    System.out.println("  " + world);
+    assert world.toString().equals("[0|0|0|3|4|5|6|7|8|9]");
+
+    world.union(5, 4);
+    System.out.println("\n  " + world);
+    assert world.toString().equals("[0|0|0|3|4|4|6|7|8|9]");
+    world.union(4, 9);
+    System.out.println("  " + world);
+    assert world.toString().equals("[0|0|0|3|9|4|6|7|8|9]");
+    world.union(7, 5);
+    System.out.println("  " + world);
+    assert !world.toString().equals("[0|0|0|3|9|4|6|5|8|9]");
+    assert world.toString().equals("[0|0|0|3|9|4|6|9|8|9]");
+    world.union(6, 8);
+    System.out.println("  " + world);
+    assert world.toString().equals("[0|0|0|3|9|4|8|9|8|9]");
+    world.union(9, 8);
+    System.out.println("  " + world);
+    assert world.toString().equals("[0|0|0|3|9|4|8|9|8|8]");
   }
 
   public static void main(String[] args) {
     System.out.println("\nRunning specs: ");
 
+
     itShouldConstruct();
     itShouldBeAbleToTraceTheRootOfANode();
+
     itShouldKnowIfTwoNodesAreConnected();
+    itShouldKnowIfTwoNodesAreTransitivelyConnected();
+    itShouldKnowIfTwoNodesAreNotConnected();
 
     // itShouldThrowOutOfMemoryError();
     itShouldComplainIfYouRunUnionOnALargeDataSet();
-    itShouldConnectTwoNodes();
+
+    itShouldPartiallyRepresentTheGraphParentNodes();
 
     System.out.println("Done\n");
   }
