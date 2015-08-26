726e73f7b7e4accdd20ae22f345569f494fac6e1
cesine <cesine@yahoo.com>
2015-08-26 18:31:29 -0400

    added binary search tree

---
+++ src/java/BinarySearchTree.java
@@ -0,0 +1,7 @@
+public class BinarySearchTree {
+
+  public BinarySearchTree(){
+
+  }
+  
+}
---
+++ src/java/BinarySearchTreeTest.java
@@ -0,0 +1,9 @@
+public class BinarySearchTreeTest {
+
+  public static void main(String[] args){
+    System.out.println("\nRunning specs for BinarySearchTree: ");
+
+    System.out.println("\nDone \n\n");
+  }
+  
+}
