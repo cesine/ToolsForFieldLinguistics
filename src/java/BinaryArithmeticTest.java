33912135e97fa8b135fd88454494476418e128d2
cesine <cesine@yahoo.com>
2015-08-27 15:23:14 -0400

    added a test client

---
+++ src/java/BinaryArithmeticTest.java
@@ -0,0 +1,10 @@
+public class BinaryArithmeticTest {
+
+  public static void main(String[] args) {
+    System.out.println("\nRunning specs for BinaryArithmetic: ");
+
+
+    System.out.println("\nDone \n\n");
+  }
+
+}
