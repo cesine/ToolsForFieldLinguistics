a8ed0ea1d3ddf60c012240950e79d1b874b88c5e
cesine <cesine@yahoo.com>
2015-08-27 15:27:08 -0400

    Added test for two functions

--- src/java/BinaryArithmetic.java
+++ src/java/BinaryArithmetic.java
@@ -2,69 +2,68 @@
 
 // // "101" "011" -> "1000"
 
- public  class  BinaryArithmetic{
+public class BinaryArithmetic {
 
- 
+  static String addBinaryStrings(String a, String b) {
+    //   int maxLength = Math.max(a.length, b.length);
+    //   char[] result = char[maxLength]; // initialize? 
 
-// String addBinaryStrings(String a, String b) {
-//   int maxLength = Math.max(a.length, b.length);
-//   char[] result = char[maxLength]; // initialize? 
-  
-//   int previousResult = 0; //itterate using a window of relevant info
-//   for (int previous = maxLength -2; previous >= 0; previous-2){
-//     int current = previous +1;
-    
-//     char acurrent = '0';
-//     char bcurrent = '0';
-   
-//     if (a.length < current){
-//       acurrent = a.charAt(current);
-//     }
-//     if (b.length < current){
-//       bcurrent = b.charAt(current);
-//     }
-//     int sum = addChar(acurrent, bcurrent);
-    
-//     if (sum > 2) {
-//       result[current] = '1';
-//      previousResult = addChar( (char) previous + '1';
-//     } if (sum == 1) {
-//       result[current] = '1';
-//     } else if (){
-//       result[current] = '0';
-//     }
+    //   int previousResult = 0; //itterate using a window of relevant info
+    //   for (int previous = maxLength -2; previous >= 0; previous-2){
+    //     int current = previous +1;
+
+    //     char acurrent = '0';
+    //     char bcurrent = '0';
+
+    //     if (a.length < current){
+    //       acurrent = a.charAt(current);
+    //     }
+    //     if (b.length < current){
+    //       bcurrent = b.charAt(current);
+    //     }
+    //     int sum = addChar(acurrent, bcurrent);
+
+    //     if (sum > 2) {
+    //       result[current] = '1';
+    //      previousResult = addChar( (char) previous + '1';
+    //     } if (sum == 1) {
+    //       result[current] = '1';
+    //     } else if (){
+    //       result[current] = '0';
+    //     }
 
 
-//     previousResult = current;
-//   }
-// }
+    //     previousResult = current;
+    //   }
+    return "";
+  }
 
-// static int addChar(char a, char b){
-//    if (a == '0' && b == '0') {
-//       return 0;
-//     } else if (a == '0' && b == '1' || a == '1' && b == '0'){
-//       return 1;
-//     } else if (a == '1' && b == '1') {
-//       return 2;
-//     }
-// }
-//                               // testing if can run java in here to run tests
-// public static int testAddChar(){
-//   int result = addChar('0', '1');
-//   assert result == 1;
-//   System.out.println(result);
-// }
-                         
+  static int addChar(char a, char b) {
+    //    if (a == '0' && b == '0') {
+    //       return 0;
+    //     } else if (a == '0' && b == '1' || a == '1' && b == '0'){
+    //       return 1;
+    //     } else if (a == '1' && b == '1') {
+    //       return 2;
+    //     }
+    // }
+    //                               // testing if can run java in here to run tests
+    // public static int testAddChar(){
+    //   int result = addChar('0', '1');
+    //   assert result == 1;
+    //   System.out.println(result);
+    return 0;
+  }
 
 
-// // something(current, positionofcarry){
 
-// //   if(pofsitionofcarry!= null){
-// //     done
-// //   }
-// //   something(positionofcarry, psotionofcarry--)
+  // // something(current, positionofcarry){
 
-// // }
-// 
+  // //   if(pofsitionofcarry!= null){
+  // //     done
+  // //   }
+  // //   something(positionofcarry, psotionofcarry--)
+
+  // // }
+  // 
 }
-
--- src/java/BinaryArithmeticTest.java
+++ src/java/BinaryArithmeticTest.java
@@ -1,8 +1,25 @@
 public class BinaryArithmeticTest {
 
+  public static void itShouldBeAbleToAddBinaryStrings(){
+    String result = BinaryArithmetic.addBinaryStrings("0","0");
+    System.out.println(" Added strings 0 and 0: "+ result);
+    assert result != null;
+  }
+
+
+  public static void itShouldBeAbleToAddChar(){
+    int result = BinaryArithmetic.addChar('0','0');
+    System.out.println(" Added chars 0 and 0: "+ result);
+    assert result ==0;
+  }
+
+
   public static void main(String[] args) {
     System.out.println("\nRunning specs for BinaryArithmetic: ");
 
+
+    itShouldBeAbleToAddBinaryStrings();
+    itShouldBeAbleToAddChar();
 
     System.out.println("\nDone \n\n");
   }
