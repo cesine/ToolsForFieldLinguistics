5fa4e5549a8cba96b500db07a4e53834708116ef
cesine <cesine@yahoo.com>
2015-08-27 10:30:08 -0400

    moved inner class to bottom
    of the file

--- src/java/BinarySearchTree.java
+++ src/java/BinarySearchTree.java
@@ -3,6 +3,50 @@ import java.util.*;
 public class BinarySearchTree {
   private Node root;
 
+
+  public BinarySearchTree() {
+
+  }
+  public boolean isEmpty() {
+    return size(root) == 0;
+  }
+
+  public int size() {
+    return size(root);
+  }
+
+  public int size(Node x) {
+    if (x == null) {
+      return 0;
+    } else {
+      return x.childrenCount;
+    }
+  }
+
+  public boolean add(String key, Object value) {
+    Node node = new Node(key, value);
+    return add(node);
+  }
+
+
+  public boolean add(String key, Object value, int childCount) {
+    Node node = new Node(key, value, childCount);
+    return add(node);
+  }
+
+  public boolean add(Node node) {
+    if (root == null) {
+      root = node;
+      return true;
+    }
+    return root.add(node);
+  }
+
+  public Node getRoot() {
+    return root;
+  }
+
+
   public static class Node {
     private String key;
     private Object value;
@@ -51,7 +95,7 @@ public class BinarySearchTree {
       } else if (compared > 0) {
         if (this.right == null) {
           this.right = node;
-          System.out.println("  Added  `" + this.key + "`'s right `" + node.key +"`");
+          System.out.println("  Added  `" + this.key + "`'s right `" + node.key + "`");
           this.childrenCount++;
         } else {
           this.right.add(node);
@@ -71,47 +115,4 @@ public class BinarySearchTree {
       return right;
     }
   }
-
-  public BinarySearchTree() {
-
-  }
-  public boolean isEmpty() {
-    return size(root) == 0;
-  }
-
-  public int size() {
-    return size(root);
-  }
-
-  public int size(Node x) {
-    if (x == null) {
-      return 0;
-    } else {
-      return x.childrenCount;
-    }
-  }
-
-  public boolean add(String key, Object value) {
-    Node node = new Node(key, value);
-    return add(node);
-  }
-
-
-  public boolean add(String key, Object value, int childCount) {
-    Node node = new Node(key, value, childCount);
-    return add(node);
-  }
-
-  public boolean add(Node node) {
-    if (root == null) {
-      root = node;
-      return true;
-    }
-    return root.add(node);
-  }
-
-  public Node getRoot() {
-    return root;
-  }
-
 }
