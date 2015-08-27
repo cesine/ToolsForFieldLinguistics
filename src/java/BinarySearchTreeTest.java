18a731e4f8e8bd2ea5a00a07f8c04cd71ee3c815
cesine <cesine@yahoo.com>
2015-08-27 10:30:24 -0400

    added test for long branches

--- src/java/BinarySearchTreeTest.java
+++ src/java/BinarySearchTreeTest.java
@@ -10,7 +10,7 @@ public class BinarySearchTreeTest {
     assert node != null;
   }
 
-  public static void itShouldAddNodes() {
+  public static void itShouldAddNodesInBallencedOrder() {
     BinarySearchTree tree = new BinarySearchTree();
     tree.add("b", null);
     System.out.println("Root: " + tree.getRoot().getKey());
@@ -18,9 +18,6 @@ public class BinarySearchTreeTest {
     // assert tree.size() == 1;
 
     tree.add("a", null);
-    System.out.println("Root: " + tree.getRoot().getKey());
-    assert "b".equals(tree.getRoot().getKey());
-
     System.out.println("Root left: " + tree.getRoot().getLeft().getKey());
     assert "a".equals(tree.getRoot().getLeft().getKey());
 
@@ -29,13 +26,32 @@ public class BinarySearchTreeTest {
     assert "c".equals(tree.getRoot().getRight().getKey());
   }
 
+  public static void itShouldAddNodesInUnballencedOrder() {
+    BinarySearchTree tree = new BinarySearchTree();
+    tree.add("a", null);
+    System.out.println("Root: " + tree.getRoot().getKey());
+    assert "a".equals(tree.getRoot().getKey());
+    // assert tree.size() == 1;
+
+    tree.add("b", null);
+    System.out.println("Root right: " + tree.getRoot().getRight().getKey());
+    assert "a".equals(tree.getRoot().getKey());
+    assert "b".equals(tree.getRoot().getRight().getKey());
+
+    tree.add("c", null);
+    System.out.println("Root right right: " + tree.getRoot().getRight().getRight().getKey());
+    assert "c".equals(tree.getRoot().getRight().getRight().getKey());
+  }
+
+
   public static void main(String[] args) {
     System.out.println("\nRunning specs for BinarySearchTree: ");
 
     itShouldConstruct();
     itShouldCreateNodes();
 
-    itShouldAddNodes();
+    // itShouldAddNodesInBallencedOrder();
+    itShouldAddNodesInUnballencedOrder();
 
     System.out.println("\nDone \n\n");
   }
