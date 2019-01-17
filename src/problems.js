1c41e6f91889f14f02b58b0a5ef930aa81a67f6f
cesine <cesine@yahoo.com>
2019-01-17 14:08:20 -0500

    update description

--- src/problems.js
+++ src/problems.js
@@ -1,2 +1,6 @@
-// 3 - Write a function that return a array of number that multiply all except itself.
-// For example: given the lists [1,2,3,4], the function should return [2*3*4, 1*3*4, 1*2*4, 1*2*3] -> [24, 12, 8, 6].
+/*
+Given an array of integers, return a new array of integers of the same size
+where each number in the array is replaced with the product of all numbers in the array except itself.
+
+For example: given the lists [1,2,3,4], the function should return [2*3*4, 1*3*4, 1*2*4, 1*2*3] -> [24, 12, 8, 6].
+*/
--- test/someTests-specs.js
+++ test/someTests-specs.js
@@ -2,7 +2,7 @@ const expect = require('expect.js');
 
 describe('Fun challenges', () => {
 
-  it('Test Mocha: ', () => {
+  it('should test expectations', () => {
     expect(true).to.be(true);
     expect(true).to.not.be(false);
     expect([1, 2, 3]).to.eql([1, 2, 3]);
