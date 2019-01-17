563419aead5112ab52e343b4074655ee8e3653d6
cesine <cesine@yahoo.com>
2019-01-17 12:23:30 -0500

    more test cases

--- test/someTests-specs.js
+++ test/someTests-specs.js
@@ -1,5 +1,7 @@
 const expect = require('expect.js');
-const { multiplyAll } = require('../src/problems');
+const {
+  multiplyAll
+} = require('../src/problems');
 
 describe('Fun challenges', () => {
   it('Test Mocha: ', () => {
@@ -9,7 +11,17 @@ describe('Fun challenges', () => {
   });
 
   it('should return [24, 12, 8, 6]', () => {
-    const result = multiplyAll([1,2,3,4]);
+    const result = multiplyAll([1, 2, 3, 4]);
     expect(result).to.eql([24, 12, 8, 6]);
+  });
+
+  it('should support arbitrary numbers', () => {
+    const result = multiplyAll([9, 22, 13, 8, 1, 3]);
+    expect(result).to.eql([6864, 2808, 4752, 7722, 61776, 20592]);
+  });
+
+  it('should support zero', () => {
+    const result = multiplyAll([9, 22, 8, 1, 0]);
+    expect(result).to.eql([0, 0, 0, 0, 1584]);
   });
 });
