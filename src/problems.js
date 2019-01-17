10c5ad2b560849bc7c4d4c9ad6faf8049c99368d
cesine <cesine@yahoo.com>
2019-01-17 12:21:35 -0500

    brute force

--- src/problems.js
+++ src/problems.js
@@ -1,2 +1,22 @@
 // 3 - Write a function that return a array of number that multiply all except itself.
 // For example: given the lists [1,2,3,4], the function should return [2*3*4, 1*3*4, 1*2*4, 1*2*3] -> [24, 12, 8, 6].
+
+
+function multiplyAll (numbers) {
+  console.log('args', numbers);
+  return numbers.map((position) => {
+    let accum = 1;
+
+    numbers.forEach((other) => {
+      if (other === position) {
+        return;
+      }
+      accum = accum * other;
+    });
+    return accum;
+  });
+}
+
+module.exports = {
+  multiplyAll
+};
--- test/someTests-specs.js
+++ test/someTests-specs.js
@@ -1,11 +1,15 @@
 const expect = require('expect.js');
+const { multiplyAll } = require('../src/problems');
 
 describe('Fun challenges', () => {
-
   it('Test Mocha: ', () => {
     expect(true).to.be(true);
     expect(true).to.not.be(false);
     expect([1, 2, 3]).to.eql([1, 2, 3]);
   });
 
+  it('should return [24, 12, 8, 6]', () => {
+    const result = multiplyAll([1,2,3,4]);
+    expect(result).to.eql([24, 12, 8, 6]);
+  });
 });
