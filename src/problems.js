ea723d4707a0def40aff27e835ab63f7c9f4c56e
cesine <cesine@yahoo.com>
2019-02-16 17:03:35 -0500

    reduce

--- src/problems.js
+++ src/problems.js
@@ -4,3 +4,12 @@ where each number in the array is replaced with the product of all numbers in th
 
 For example: given the lists [1,2,3,4], the function should return [2*3*4, 1*3*4, 1*2*4, 1*2*3] -> [24, 12, 8, 6].
 */
+
+module.exports = function productExceptIndex(input) {
+  const initialValue = 1;
+  return input.map(function(val, i) {
+    return input.reduce(function(acc, curr, j) {
+      return (i === j) ? acc : (acc * curr);
+    }, initialValue);
+  });
+};
