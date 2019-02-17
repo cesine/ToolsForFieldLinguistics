7bf30e4539c96187a3b615a343531080c3ed1d94
cesine <cesine@yahoo.com>
2019-02-16 19:42:20 -0500

    add nested loops, map and reduce examples

--- test/javascript/explore-data/accumulation-spec.js
+++ test/javascript/explore-data/accumulation-spec.js
@@ -14,7 +14,45 @@ var debug = function() {
  * @param  {Array} input Array of integers
  * @return {Array}       Array of integers
  */
-function productExceptIndex(input) {
+function productExceptSelfUsingNestedLoops(input) {
+  var result = [];
+  for (var i = 0; i < input.length; i++) {
+    var product = 1;
+    for (var j = 0; j < input.length; j++) {
+      if (i !== j) {
+        product = product * input[j];
+      }
+    }
+    result.push(product);
+  }
+  return result;
+}
+
+function productExceptSelfUsingNestedMap(input) {
+  return input.map(function(position, i) {
+    var accum = 1;
+
+    input.forEach(function(other, j) {
+      if (i === j) {
+        return;
+      }
+      accum = accum * other;
+    });
+
+    return accum;
+  });
+}
+
+function productExceptSelfUsingNestedReduce(input) {
+  var initialValue = 1;
+  return input.map(function(val, i) {
+    return input.reduce(function(acc, curr, j) {
+      return (i === j) ? acc : (acc * curr);
+    }, initialValue);
+  });
+}
+
+function productExceptSelfLinear(input) {
   var i;
   // Accumulate products of values to the right
   var right = [1];
@@ -38,17 +76,27 @@ function productExceptIndex(input) {
 
 describe("Accumulation", function() {
   it("should return [24, 12, 8, 6]", function() {
-    var result = productExceptIndex([1, 2, 3, 4]);
+    var result = productExceptSelfLinear([1, 2, 3, 4]);
     expect(result).toEqual([24, 12, 8, 6]);
+    expect(productExceptSelfUsingNestedLoops([1, 2, 3, 4])).toEqual(result);
+    expect(productExceptSelfUsingNestedMap([1, 2, 3, 4])).toEqual(result);
+    expect(productExceptSelfUsingNestedReduce([1, 2, 3, 4])).toEqual(result);
   });
 
   it("should support arbitrary numbers", function() {
-    var result = productExceptIndex([9, 22, 13, 8, 1, 3]);
+    var result = productExceptSelfLinear([9, 22, 13, 8, 1, 3]);
     expect(result).toEqual([6864, 2808, 4752, 7722, 61776, 20592]);
+
+    expect(productExceptSelfUsingNestedLoops([9, 22, 13, 8, 1, 3])).toEqual(result);
+    expect(productExceptSelfUsingNestedMap([9, 22, 13, 8, 1, 3])).toEqual(result);
+    expect(productExceptSelfUsingNestedReduce([9, 22, 13, 8, 1, 3])).toEqual(result);
   });
 
   it("should support zero", function() {
-    var result = productExceptIndex([9, 22, 8, 1, 0]);
+    var result = productExceptSelfLinear([9, 22, 8, 1, 0]);
     expect(result).toEqual([0, 0, 0, 0, 1584]);
+    expect(productExceptSelfUsingNestedLoops([9, 22, 8, 1, 0])).toEqual(result);
+    expect(productExceptSelfUsingNestedMap([9, 22, 8, 1, 0])).toEqual(result);
+    expect(productExceptSelfUsingNestedReduce([9, 22, 8, 1, 0])).toEqual(result);
   });
 });
