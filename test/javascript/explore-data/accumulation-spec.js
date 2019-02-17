5d27f886b32bfcae8a1b7b844d8ad3a420f2b3f4
cesine <cesine@yahoo.com>
2019-02-16 19:47:41 -0500

    consitant variable names

--- test/javascript/explore-data/accumulation-spec.js
+++ test/javascript/explore-data/accumulation-spec.js
@@ -11,28 +11,31 @@ var debug = function() {
  * For example: given the lists [1,2,3,4], the function
  * should return [2*3*4, 1*3*4, 1*2*4, 1*2*3] -> [24, 12, 8, 6].
  *
- * @param  {Array} input Array of integers
+ * @param  {Array} numbers Array of integers
  * @return {Array}       Array of integers
  */
-function productExceptSelfUsingNestedLoops(input) {
+function productExceptSelfUsingNestedLoops(numbers) {
   var result = [];
-  for (var i = 0; i < input.length; i++) {
-    var product = 1;
-    for (var j = 0; j < input.length; j++) {
-      if (i !== j) {
-        product = product * input[j];
+  for (var i = 0; i < numbers.length; i++) {
+    var accum = 1;
+
+    for (var j = 0; j < numbers.length; j++) {
+      if (i === j) {
+        continue;
       }
+      accum = accum * numbers[j];
     }
-    result.push(product);
+
+    result.push(accum);
   }
   return result;
 }
 
-function productExceptSelfUsingNestedMap(input) {
-  return input.map(function(position, i) {
+function productExceptSelfUsingNestedMap(numbers) {
+  return numbers.map(function(position, i) {
     var accum = 1;
 
-    input.forEach(function(other, j) {
+    numbers.forEach(function(other, j) {
       if (i === j) {
         return;
       }
@@ -43,33 +46,37 @@ function productExceptSelfUsingNestedMap(input) {
   });
 }
 
-function productExceptSelfUsingNestedReduce(input) {
+function productExceptSelfUsingNestedReduce(numbers) {
   var initialValue = 1;
-  return input.map(function(val, i) {
-    return input.reduce(function(acc, curr, j) {
-      return (i === j) ? acc : (acc * curr);
+  return numbers.map(function(val, i) {
+    return numbers.reduce(function(acc, curr, j) {
+      if (i === j) {
+        return acc;
+      }
+
+      return acc * curr;
     }, initialValue);
   });
 }
 
-function productExceptSelfLinear(input) {
+function productExceptSelfLinear(numbers) {
   var i;
   // Accumulate products of values to the right
   var right = [1];
-  for (i = input.length - 1; i > 0; i--) {
-    right.unshift(right[0] * input[i]);
+  for (i = numbers.length - 1; i > 0; i--) {
+    right.unshift(right[0] * numbers[i]);
   }
   debug("right", right);
 
   // Accumulate products of values to the left
   var left = [1];
-  for (i = 0; i < input.length - 1; i++) {
-    left.push(left[i] * input[i]);
+  for (i = 0; i < numbers.length - 1; i++) {
+    left.push(left[i] * numbers[i]);
   }
   debug("left", left);
 
   // Multiply right and left
-  return input.map(function(val, i) {
+  return numbers.map(function(val, i) {
     return right[i] * left[i];
   });
 }
@@ -86,7 +93,6 @@ describe("Accumulation", function() {
   it("should support arbitrary numbers", function() {
     var result = productExceptSelfLinear([9, 22, 13, 8, 1, 3]);
     expect(result).toEqual([6864, 2808, 4752, 7722, 61776, 20592]);
-
     expect(productExceptSelfUsingNestedLoops([9, 22, 13, 8, 1, 3])).toEqual(result);
     expect(productExceptSelfUsingNestedMap([9, 22, 13, 8, 1, 3])).toEqual(result);
     expect(productExceptSelfUsingNestedReduce([9, 22, 13, 8, 1, 3])).toEqual(result);
