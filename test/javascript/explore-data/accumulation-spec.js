5dcf488373bd1e0a17ce24f9e9d7e723a2d4d572
cesine <cesine@yahoo.com>
2019-02-16 19:28:57 -0500

    reduce logging

--- test/javascript/explore-data/accumulation-spec.js
+++ test/javascript/explore-data/accumulation-spec.js
@@ -1,3 +1,8 @@
+var debug = function() {
+  // console.log(arguments);
+  return;
+};
+
 /**
  * Given an array of integers, return a new array of integers of the same size
  * where each number in the array is replaced with the product of all numbers
@@ -16,33 +21,33 @@ function productExceptIndex(input) {
   for (i = input.length - 1; i > 0; i--) {
     right.unshift(right[0] * input[i]);
   }
-  console.log('right', right);
+  debug("right", right);
 
   // Accumulate products of values to the left
   var left = [1];
   for (i = 0; i < input.length - 1; i++) {
     left.push(left[i] * input[i]);
   }
-  console.log('left', left);
+  debug("left", left);
 
   // Multiply right and left
   return input.map(function(val, i) {
     return right[i] * left[i];
   });
-};
+}
 
-describe('Accumulation', function() {
-  it('should return [24, 12, 8, 6]', function() {
+describe("Accumulation", function() {
+  it("should return [24, 12, 8, 6]", function() {
     var result = productExceptIndex([1, 2, 3, 4]);
     expect(result).toEqual([24, 12, 8, 6]);
   });
 
-  it('should support arbitrary numbers', function() {
+  it("should support arbitrary numbers", function() {
     var result = productExceptIndex([9, 22, 13, 8, 1, 3]);
     expect(result).toEqual([6864, 2808, 4752, 7722, 61776, 20592]);
   });
 
-  it('should support zero', function() {
+  it("should support zero", function() {
     var result = productExceptIndex([9, 22, 8, 1, 0]);
     expect(result).toEqual([0, 0, 0, 0, 1584]);
   });
